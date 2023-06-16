ea1bc93a01b13d857b5c629592e15aad2e12c21b
HerrKermet <david.stechow1113@gmail.com>
2023-06-16 12:18:25 +0200

    adjusted test cases

--- HA 4 Halde/halde/test.c
+++ HA 4 Halde/halde/test.c
@@ -12,18 +12,51 @@ int main(int argc, char *argv[]) {
     
     char* m2 = malloc(48);
     printList();
+    
+    char* m3 = malloc(80);
+    printList();
+    
+    char* m4 = malloc(16);
+    printList();    
 
     free(m1);
     printList();
     
-    char* m3 = malloc(80);
-    printList();
     
     free(m2);
     printList();
     
     free(m3);
     printList();
+    
+    free(m4);
+    printList();
+    
+    m1 = malloc(1024);
+    printList();
+    
+    m2 = malloc(16);
+    printList();
+    
+    m3 = malloc(47);
+    printList();
+    
+    m4 = malloc(64);
+    printList();  
+    
+    free(m1);
+    printList();
+    
+    
+    free(m2);
+    printList();
+    
+    free(m3);
+    printList();
+    
+    free(m4);
+    printList();
+    
 
     exit(EXIT_SUCCESS);
 }
